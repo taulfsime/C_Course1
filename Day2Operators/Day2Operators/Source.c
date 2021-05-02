185ac561e3c59fdf33a04563917bbdbe9c57a6b4
taulfsime <kris.td@mail.bg>
2021-05-02 22:08:05 +0300

    Some changes

--- Day2Operators/Day2Operators/Source.c
+++ Day2Operators/Day2Operators/Source.c
@@ -2,17 +2,20 @@
 #include <stdio.h>
 #include <math.h>
 
+void solve(double meal_cost, int tip_percent, int tax_percent) {
+	float tip = meal_cost * ((float)tip_percent / 100.0f);
+	float tax = meal_cost * ((float)tax_percent / 100.0f);
+
+	printf("%d", (int)round(meal_cost + tip + tax));
+}
+
 int main() {
 	float price;
 	int tipPer;
 	int taxPer;
 
 	scanf("%f\n%d\n%d", &price, &tipPer, &taxPer);
-
-	float tip = price * ((float)tipPer / 100.0f);
-	float tax = price * ((float)taxPer / 100.0f);
-
-	printf("%d", (int)round(price + tip + tax));
+	solve(price, tipPer, taxPer);
 
 	return 0;
 }
